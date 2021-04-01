bad53ecccb67131f5f8a7d055813b0ab0aaf762a
Asha Sanford <sanford.asha97@yahoo.com>
2021-04-01 16:55:41 -0400

    added yoga button countdowns

--- page-3/script-desktop3.js
+++ page-3/script-desktop3.js
@@ -10,6 +10,8 @@ var yoga6 = document.querySelector("#yoga6");
 
 
 
+// var timers = document.querySelectorAll(".timer");
+// var secondsLeft = 30;
 
 
 var quote = document.querySelector("#quote")
@@ -18,15 +20,140 @@ var playBtn2 = document.querySelector("#pause");
 var player = document.querySelector("#player");
 
 yoga1.addEventListener("click", clicked);
-yoga2.addEventListener("click", clicked);
-yoga3.addEventListener("click", clicked);
-yoga4.addEventListener("click", clicked);
-yoga5.addEventListener("click", clicked);
-yoga6.addEventListener("click", clicked);
+yoga2.addEventListener("click", clickedOne);
+yoga3.addEventListener("click", clickedTwo);
+yoga4.addEventListener("click", clickedThree);
+yoga5.addEventListener("click", clickedFour);
+yoga6.addEventListener("click", clickedFive);
 
 function clicked() {
   console.log("i was clicked");
+  startTimer();
 }
+
+function clickedOne() {
+  startTimerOne();
+}
+
+function clickedTwo() {
+  startTimerTwo();
+}
+
+function clickedThree() {
+  startTimerThree();
+}
+
+function clickedFour() {
+  startTimerFour();
+}
+function clickedFive() {
+  startTimerFive();
+}
+
+// function startTimer() {
+    
+//   var timer = setInterval(function() {
+//   secondsLeft--;
+//   timers[0].textContent = secondsLeft;
+//   if (secondsLeft === 0) {
+//       clearInterval(timer);
+//   };
+
+// }, 1000);
+
+// }
+
+function startTimer() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[0]--;
+    timers[0].textContent = secondsLeft[0];
+    if (secondsLeft[0] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+function startTimerOne() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[1]--;
+    timers[1].textContent = secondsLeft[1];
+    if (secondsLeft[1] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+function startTimerTwo() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[2]--;
+    timers[2].textContent = secondsLeft[2];
+    if (secondsLeft[2] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+function startTimerThree() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[3]--;
+    timers[3].textContent = secondsLeft[3];
+    if (secondsLeft[3] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+function startTimerFour() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[4]--;
+    timers[4].textContent = secondsLeft[4];
+    if (secondsLeft[4] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+function startTimerFive() {
+  var timers = document.querySelectorAll(".timer");
+var secondsLeft = [30,30,30,30,30,30];
+  var timer = setInterval(function() {  
+    
+    secondsLeft[5]--;
+    timers[5].textContent = secondsLeft[5];
+    if (secondsLeft[5] === 0) {
+        clearInterval(timer);
+    };
+  
+  }, 1000);
+
+}
+
+
 
 
 var quotesUrl = "https://type.fit/api/quotes";
